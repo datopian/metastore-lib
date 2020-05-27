fee39846fc6f373fd3431e79b7e0d16f546b5197
Shahar Evron <shahar.evron@gmail.com>
2020-05-27 16:01:00 +0300

    Add basic test for factory function

---
+++ tests/test_base.py
@@ -0,0 +1,15 @@
+"""Base library tests
+"""
+from metastore.backend import create_metastore
+from metastore.backend.filesystem import FilesystemStorage
+from metastore.backend.gh import GitHubStorage
+
+
+def test_factory_instantiates_filesystem_backend():
+    backend = create_metastore('filesystem', dict(uri='mem://'))
+    assert isinstance(backend, FilesystemStorage)
+
+
+def test_factory_instantiates_github_backend():
+    backend = create_metastore('github', {})
+    assert isinstance(backend, GitHubStorage)
