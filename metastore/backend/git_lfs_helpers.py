d1c503a33937428c7a6110d83928ba08fad8b20f
Shahar Evron <shahar.evron@gmail.com>
2020-06-10 10:21:33 +0300

    Implement LFS config and pointer file creation
    This closes #5

--- metastore/backend/git_lfs_helpers.py
+++ metastore/backend/git_lfs_helpers.py
@@ -1,12 +1,70 @@
 """Git LFS Helpers used by all Git based backends that support Git-LFS
 """
-from typing import List, Union
+from typing import Any, Dict, Iterable, Union
 
 from six import string_types
 
+from ..util import is_hex_str
 
-def generate_gitattributes(track_files):
-    # type: (Union[str, List[str]]) -> str
+
+def is_posix_path_resource(resource):
+    # type: (Dict[str, Any]) -> bool
+    """Tell if a resource represents a POSIX-path (i.e. local file) resource
+
+    >>> is_posix_path_resource({"path": "data/file.csv"})
+    True
+
+    >>> is_posix_path_resource({"path": "a file with some stange name"})
+    True
+
+    >>> is_posix_path_resource({"path": "http://example.com/my-resource"})
+    False
+
+    >>> is_posix_path_resource({"data": "some-inline-data"})
+    False
+
+    >>> is_posix_path_resource({"path": ["file1.csv", "file2.csv"]})
+    False
+    """
+    if 'path' not in resource:
+        return False
+
+    if not isinstance(resource['path'], str):
+        return False
+
+    if resource['path'][:7] in {'http://', 'https:/'}:
+        return False
+
+    return True
+
+
+def has_lfs_attributes(resource):
+    # type: (Dict[str, Any]) -> bool
+    """Tell if a resource has the attributes required for an LFS-stored resource
+
+    >>> has_lfs_attributes({"path": "data.csv", "size": 1234, "sha256": "someshavalue"})
+    True
+
+    >>> has_lfs_attributes({"path": "data.csv", "sha256": "someshavalue"})
+    False
+
+    >>> has_lfs_attributes({"path": "data.csv", "sha256": "someshavalue"})
+    False
+
+    >>> has_lfs_attributes({"path": "data.csv"})
+    False
+    """
+    if not isinstance(resource.get('sha256'), str):
+        return False
+
+    if not isinstance(resource.get('size'), int):
+        return False
+
+    return True
+
+
+def create_git_attributes_file(track_files):
+    # type: (Union[str, Iterable[str]]) -> str
     """Create the contents of a .gitattributes file as required by Git LFS
     """
     if isinstance(track_files, string_types):
@@ -14,3 +72,22 @@ def generate_gitattributes(track_files):
 
     lines = ['{} filter=lfs diff=lfs merge=lfs -text\n'.format(p) for p in track_files]
     return ''.join(lines)
+
+
+def create_lfs_config_file(lfs_server_url, remote='origin'):
+    # type: (str, str) -> str
+    """Create contents of .lfsconfig file
+    """
+    return '[remote "{remote}"]\n\tlfsurl = {lfsurl}'.format(remote=remote, lfsurl=lfs_server_url)
+
+
+def create_lfs_pointer_file(resource):
+    # type: (Dict[str, Any]) -> str
+    """Create contents for LFS pointer file
+    """
+    if not is_hex_str(resource['sha256'], chars=64):
+        raise ValueError('Resource sha256 value does not seem to be a valid sha256 hex string')
+
+    return ('version https://git-lfs.github.com/spec/v1\n'
+            'oid sha256:{}\n'
+            'size {}\n').format(resource['sha256'], resource['size'])
--- tests/backend/test_git_lfs_helpers.py
+++ tests/backend/test_git_lfs_helpers.py
@@ -1,14 +1,49 @@
 """Tests for the Git LFS helpers module
 """
+import pytest
+
 from metastore.backend import git_lfs_helpers
 
 
-def test_generate_gitattributes():
+def test_create_git_attributes():
     expected = 'data/* filter=lfs diff=lfs merge=lfs -text\n'
-    assert expected == git_lfs_helpers.generate_gitattributes('data/*')
+    assert expected == git_lfs_helpers.create_git_attributes_file('data/*')
 
 
-def test_generate_gitattributes_multi_patterns():
+def test_create_git_attributes_multi_patterns():
     expected = ('data/*.csv filter=lfs diff=lfs merge=lfs -text\n'
                 'data/*.xls filter=lfs diff=lfs merge=lfs -text\n')
-    assert expected == git_lfs_helpers.generate_gitattributes(['data/*.csv', 'data/*.xls'])
+    assert expected == git_lfs_helpers.create_git_attributes_file(['data/*.csv', 'data/*.xls'])
+
+
+def test_create_lfs_config_file():
+    expected = '[remote "origin"]\n\tlfsurl = https://lfs.example.com/foo/bar'
+    assert expected == git_lfs_helpers.create_lfs_config_file('https://lfs.example.com/foo/bar')
+
+
+def test_create_lfs_config_file_custom_remote_name():
+    expected = '[remote "baz-baz"]\n\tlfsurl = https://lfs.example.com/baz/baz'
+    assert expected == git_lfs_helpers.create_lfs_config_file('https://lfs.example.com/baz/baz', remote='baz-baz')
+
+
+def create_lfs_pointer_file():
+    resource = {"sha256": "0f1128046248f83dc9b9ab187e16fad0ff596128f1524d05a9a77c4ad932f10a",
+                "size": 1744,
+                "path": "data/resource.csv"}
+
+    expected = ('version https://git-lfs.github.com/spec/v1\n'
+                'oid sha256:0f1128046248f83dc9b9ab187e16fad0ff596128f1524d05a9a77c4ad932f10a\n'
+                'size 1744\n')
+
+    assert expected == git_lfs_helpers.create_lfs_pointer_file(resource)
+
+
+@pytest.mark.parametrize('resource', [
+    {"size": 1744, "path": "data/resource.csv"},
+    {"sha256": "0f1128046248f83dc9b9ab187e16fad0ff596128f1524d05a9a77c4ad932f10a"},
+    {"size": 1744, "sha256": "ef42bab1191da272f13935f78c401e3de0c11afb"},
+    {"size": 1744, "sha256": "0f112804X248f83dc9b9ab187e16fad0ff596128f1524d05a9a77c4ad932f10a"},
+])
+def create_lfs_pointer_file_invalid_resource(resource):
+    with pytest.raises(ValueError):
+        git_lfs_helpers.create_lfs_pointer_file(resource)
