fb178471eee41cfcd8381be78b818c4599298b41
Shahar Evron <shahar.evron@gmail.com>
2020-06-10 10:20:24 +0300

    Generalize is_revision_like function

--- metastore/util.py
+++ metastore/util.py
@@ -23,3 +23,39 @@ def get_callable(callable_str, base_package=None):
         raise ValueError("Expecting base_package to be set if only class name is provided")
 
     return getattr(module, callable_name)  # type: ignore
+
+
+def is_hex_str(value, chars=40):
+    # type: (str, int) -> bool
+    """Check if a string is a hex-only string of exactly :param:`chars` characters length.
+
+    This is useful to verify that a string contains a valid SHA, MD5 or UUID-like value.
+
+    >>> is_hex_str('0f1128046248f83dc9b9ab187e16fad0ff596128f1524d05a9a77c4ad932f10a', 64)
+    True
+
+    >>> is_hex_str('0f1128046248f83dc9b9ab187e16fad0ff596128f1524d05a9a77c4ad932f10a', 32)
+    False
+
+    >>> is_hex_str('0f1128046248f83dc9b9ab187e1xfad0ff596128f1524d05a9a77c4ad932f10a', 64)
+    False
+
+    >>> is_hex_str('ef42bab1191da272f13935f78c401e3de0c11afb')
+    True
+
+    >>> is_hex_str('ef42bab1191da272f13935f78c401e3de0c11afb'.upper())
+    True
+
+    >>> is_hex_str('ef42bab1191da272f13935f78c401e3de0c11afb', 64)
+    False
+
+    >>> is_hex_str('ef42bab1191da272f13935.78c401e3de0c11afb')
+    False
+    """
+    if len(value) != chars:
+        return False
+    try:
+        int(value, 16)
+    except ValueError:
+        return False
+    return True
